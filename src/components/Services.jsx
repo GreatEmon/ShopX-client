7c191bdd9c4f6e07bb6b2e51ddeac1a4d391ef89
GreatEmon <contact.emonpro@gmail.com>
2025-09-27 13:33:16 +0600

    edited

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -37,6 +37,7 @@ const Services = () => {
                 </div>
             </div>
     </>
+    
   )
 }
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Slider = ({image,title,desc}) => {
+    
     return (
         <div>
             <div
