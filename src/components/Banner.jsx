7e4d9389785d3496f5c48bbc56d97826c5c661a8
GreatEmon <contact.emonpro@gmail.com>
2025-09-27 13:19:18 +0600

    edited

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,22 +7,22 @@ import { Navigation, Pagination } from 'swiper/modules';
 import Slider from './Slider';
 
 const Banner = () => {
-    return (
-        <div className='my-4'>
-            <Swiper
+  return (
+    <div className='my-4'>
+      <Swiper
         spaceBetween={30}
         pagination={{
           clickable: true,
         }}
-        modules={[Pagination,Navigation]}
-        navigation = {true}
+        modules={[Pagination, Navigation]}
+        navigation={true}
       >
         <SwiperSlide><Slider image="https://images.pexels.com/photos/6116877/pexels-photo-6116877.jpeg" title="Fresh Finds, Just In!" desc="Discover the latest trends and must-have products, handpicked for your style."></Slider></SwiperSlide>
         <SwiperSlide><Slider image="https://images.pexels.com/photos/6116892/pexels-photo-6116892.jpeg" title="Big Savings, Limited Time" desc="Grab your favorites at unbeatable prices before the season ends."></Slider></SwiperSlide>
         <SwiperSlide><Slider image="https://images.pexels.com/photos/5926462/pexels-photo-5926462.jpeg" title="Luxury You Deserve" desc="Experience top-quality products designed to elevate your everyday life."></Slider></SwiperSlide>
       </Swiper>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Banner
