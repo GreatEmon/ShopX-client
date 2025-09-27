90a4bc05d6b7e677c1c9639cf83ef6415742c04e
GreatEmon <contact.emonpro@gmail.com>
2025-09-27 13:21:41 +0600

    edited

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -62,7 +62,7 @@ const Register = () => {
 
   const handleGoogle = () => {
     logwGoogle().then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
+
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
       // The signed-in user info.
@@ -70,7 +70,7 @@ const Register = () => {
       setUser(setUser)
       navigate('/')
       // IdP data available using getAdditionalUserInfo(result)
-      // ...
+
     }).catch((error) => {
       // Handle Errors here.
       const errorCode = error.code;
@@ -79,7 +79,6 @@ const Register = () => {
       const email = error.customData.email;
       // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
     });  
   }
 
